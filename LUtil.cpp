1750c757270aff1ff735747a502e4667d19528ac
Ashwin <ashwin.mirskar@gmail.com>
2020-03-26 15:42:40 +0530

    colorful now

--- LUtil.cpp
+++ LUtil.cpp
@@ -2,7 +2,8 @@
 
 #define pi 3.142857 
 
-
+//SCORE 1 point everytime you hit 
+int score=0;
 
 //"gColorMode" controls whether we render a solid cyan square or a multicolored square.
 int gColorMode = COLOR_MODE_MULTI;
@@ -23,12 +24,18 @@ int right_wall=SCREEN_WIDTH-50;
 int factor=0,paddle_left=0,paddle_right=0;
 
 
+//indicating change in x and y directions
+//Dictates ball speed
+int delta_x=10,delta_y=15;
+
 //x and y for ball center
 int x=50,y=SCREEN_HEIGHT-50-PADDLE_HEIGHT ;
 //h and v are factors to hold +ve/-ve signs for movement directions
 int h=1,v=-1;
 //to chk if ball is movin up/down or right/left
 int flag_down=-1,flag_left=-1; // (h/v)*cor-ord value much give appropriate dir wrt the x/y axes
+
+
 
 bool initGL()
 {
@@ -63,7 +70,9 @@ void update()
 }
 
 void DrawCircle(float cx, float cy, float r, int num_segments) {
-    glBegin(GL_LINE_LOOP);
+    glColor3f(0,1,0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
     for (int ii = 0; ii < num_segments; ii++)   {
         float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle 
         float x = r * cosf(theta);//calculate the x component 
@@ -105,7 +114,7 @@ void render()
 
         //Moveable Paddle
         
-        glBegin(GL_LINE_LOOP);
+        glBegin(GL_POLYGON);
 
             glColor3f(1,0,1);
             glLineWidth(5.0f);
@@ -133,16 +142,13 @@ void render()
         
         //Game Physics - Ball
 
-        //indicating change in x and y directions
-        int delta_x=5,delta_y=10;
-              
-        
-        int ctr=0;
+        //speeding up the ball
 
         DrawCircle(x,y,BALL_RADII,8);
         x=x+(delta_x * h) ;
         y=y+(delta_y * v) ;
 
+        
 
       
             int i=0;
@@ -166,7 +172,7 @@ void render()
             }
 
             //CHECK TOP WALL IMPACT
-            if(y==-(SCREEN_HEIGHT-50) && flag_down==-1)//0
+            if(y<=-(SCREEN_HEIGHT-50) && flag_down==-1)//0
             {
                 flag_down=1;
                 v=1;
@@ -176,7 +182,8 @@ void render()
 
             //CHECK BOTTOME PADDLE IMPACT
             if( (x > paddle_left && x < paddle_right) && y==SCREEN_HEIGHT-50-PADDLE_HEIGHT && flag_down==1 )
-            {   printf("HIT\n");
+            {   printf("delta_y=%d \t HIT\n",delta_y);
+                score++;
                 flag_down=-1;
                 v=-1;
                 
@@ -196,6 +203,7 @@ void render()
             {
                //printf("x=%d  \t  paddle_left=%d \t paddle_right=%d\n",x,paddle_left,paddle_right);
                printf("GAME OVER\n");
+               printf("SCORE : %d\n",score);
                exit(0);
             }
 
